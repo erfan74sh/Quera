4d37db86eda7821c4aed14f92638be4dbbcde217
erfan shafiee <erfan74sh@gmail.com>
2022-05-26 15:30:55 +0430

    create checkRoutes function

--- client-router/clientRouter.js
+++ client-router/clientRouter.js
@@ -1,45 +1,68 @@
 let routes = null;
 let routeContainer = null;
 
+function checkRoutes(route) {
+	const keys = [];
+	route = route.replace(/:(\w+)/g, (_, key) => {
+		keys.push(key);
+		return "([^\\/]+)";
+	});
+
+	const source = `^(${route})`;
+
+	const regex = new RegExp(source, "i");
+	return { regex, keys };
+}
+
 function processRoutes() {
-  const currentRoute = window.location.pathname;
+	const currentRoute = window.location.pathname;
 
-  if (!routeContainer) return;
+	if (!routeContainer) return;
 
-  if (routes[currentRoute]) {
-    // Normal route without params
-  } else {
-    // Routes containing params
-    // No match found, show 404 page:
-  }
+	if (routes[currentRoute]) {
+		// Normal route without params
+		console.log(routeContainer);
+		routeContainer.innerHTML = routes[currentRoute]();
+	} else {
+		// Routes containing params
+		console.log(currentRoute);
+		const keys = [];
+		for (let route in routes) {
+			route = route.replace(/:(\w+)/g, (_, key) => {
+				keys.push(key);
+				return "([^\\/]+)";
+			});
+		}
+		// No match found, show 404 page:
+	}
 
-  handleLinks();
+	handleLinks();
 }
 
 /**
  * Add event listeners to each custom link
  */
 function handleLinks() {
-  const links = document.querySelectorAll("a[data-href]");
-  // Handle link click events
+	const links = document.querySelectorAll("a[data-href]");
+	// Handle link click events
 }
 
 function handleLinkClick(e) {
-  e.preventDefault();
-  if (e.currentTarget.dataset.href) {
-    // Handle link click
-  }
+	e.preventDefault();
+	if (e.currentTarget.dataset.href) {
+		// Handle link click
+	}
 }
 
 function handleRouteChange() {
-  // handle route change when user clicks the browser's back and forward buttons
-  window.onpopstate = processRoutes;
+	// handle route change when user clicks the browser's back and forward buttons
+	window.onpopstate = processRoutes;
 }
 
 export function initializeRouter(routeList, container) {
-  routes = routeList;
-  routeContainer = container;
+	routes = routeList;
+	routeContainer = container;
 
-  processRoutes();
-  handleRouteChange();
+	processRoutes();
+	handleRouteChange();
 }
